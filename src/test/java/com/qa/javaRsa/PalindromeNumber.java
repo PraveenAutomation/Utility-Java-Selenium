d6f82941aebce8656906037fb72e72c83988a037
PraveenDataScience <praveen.mldatascience@gmail.com>
2021-05-20 21:48:29 +0530

    Added Palindrome number

--- src/test/java/com/qa/javaRsa/PalindromeNumber.java
+++ src/test/java/com/qa/javaRsa/PalindromeNumber.java
@@ -4,23 +4,11 @@ public class PalindromeNumber {
 
 	public static void main(String[] args) {
 		
-		int num=121,rem,rev=0;
-		int temp=num;
-		
+		int num=121,rev=0;
 		
 		while(num!=0)
 		{
-			//rem=temp%10;
-			rev=rev*10+num%10;
-			num=num/10;
-		}
-		if(rev==num)
-		{
-			System.out.println("Palindrome");
-		}
-		else
-		{
-			System.out.println("Not Palindrome");
+			
 		}
 
 	}
