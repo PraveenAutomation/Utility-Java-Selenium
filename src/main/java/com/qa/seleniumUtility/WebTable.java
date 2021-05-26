b8f2dfd13035d3842fd218a67ba562c85a27b11b
PraveenDataScience <praveen.mldatascience@gmail.com>
2021-05-26 21:48:35 +0530

    Added array sort & product utility

--- src/main/java/com/qa/seleniumUtility/WebTable.java
+++ src/main/java/com/qa/seleniumUtility/WebTable.java
@@ -1,9 +1,26 @@
 package com.qa.seleniumUtility;
 
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class WebTable {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver=new ChromeDriver();
+		
+		driver.manage().window().maximize();
+		driver.manage().deleteAllCookies();
+		driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(10));
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		
+		driver.get("https://www.techlistic.com/p/demo-selenium-practice.html");
+		
+		
 
 	}
 
