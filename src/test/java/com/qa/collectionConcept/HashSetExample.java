5914348aa94a08d01b9ec5e5cadfa871f18179d2
PraveenDataScience <praveen.mldatascience@gmail.com>
2021-06-07 19:10:25 +0530

    Addred rest part of HashSet class

--- src/test/java/com/qa/collectionConcept/HashSetExample.java
+++ src/test/java/com/qa/collectionConcept/HashSetExample.java
@@ -1,9 +1,13 @@
 package com.qa.collectionConcept;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class HashSetExample {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Set<String> se = new HashSet<String>();
+		
 
 	}
 
