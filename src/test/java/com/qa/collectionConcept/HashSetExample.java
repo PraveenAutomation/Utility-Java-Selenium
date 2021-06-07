386bf1f46fb8432a061e6c6d4b6992d1e83c86db
PraveenAutomation <praveen.seleniumautomation@gmail.com>
2021-06-07 22:38:28 +0530

    Completed HashSet class

--- src/test/java/com/qa/collectionConcept/HashSetExample.java
+++ src/test/java/com/qa/collectionConcept/HashSetExample.java
@@ -1,6 +1,7 @@
 package com.qa.collectionConcept;
 
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Set;
 
 public class HashSetExample {
@@ -8,6 +9,24 @@ public class HashSetExample {
 	public static void main(String[] args) {
 		
 		Set<String> se = new HashSet<String>();
+		se.add("Ram");
+		se.add("VDS");
+		se.add("TECH");
+		se.add("LABS");
+		se.add("star");
+		System.out.println(se);
+		
+		for(String s : se)
+		{
+			System.out.println(s);
+		}
+		
+		Iterator itr=se.iterator();
+		System.out.println("=============================");
+		while(itr.hasNext())
+		{
+			System.out.println(itr.next());
+		}
 
 		
 
