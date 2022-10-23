f318c5536ba992024651490a984ecc7ac6efcf99
PraveendataScience <praveen.mldatascience@gmail.com>
2022-10-23 13:14:44 +0530

    About wrapper classes

---
+++ WrapperObjective.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class WrapperObjective
+{
+	public static void main(String... args)
+	{
+		ArrayList l=new ArrayList();
+		System.out.println(l.add(10));
+	}
+}
---
+++ src/test/java/com/qa/wrapperClasses/Objective_Of_Wrapper.java
@@ -0,0 +1,16 @@
+package com.qa.wrapperClasses;
+
+import java.util.ArrayList;
+
+public class Objective_Of_Wrapper {
+
+	public static void main(String[] args) {
+		
+		ArrayList al=new ArrayList();
+		al.add(10);
+		
+		
+
+	}
+
+}
