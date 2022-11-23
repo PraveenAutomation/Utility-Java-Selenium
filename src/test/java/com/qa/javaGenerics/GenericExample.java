93614c6fd9ce18102f59b95e74a2edaefbdac90c
PraveendataScience <praveen.mldatascience@gmail.com>
2022-11-23 12:56:38 +0530

    Adding Generics concepts

---
+++ src/test/java/com/qa/javaGenerics/GenericExample.java
@@ -0,0 +1,12 @@
+package com.qa.javaGenerics;
+
+public class GenericExample {
+
+	public static void main(String[] args) {
+		
+		GenericConstructor obj1=new GenericConstructor(10);
+		GenericConstructor obj2=new GenericConstructor("Prya");
+		GenericConstructor obj3=new GenericConstructor(55.58);
+	}
+
+}
