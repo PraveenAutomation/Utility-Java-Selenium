a894b8ac4b8e96ad70206918ae43b3b43d11be71
PraveendataScience <praveen.mldatascience@gmail.com>
2023-08-28 11:21:07 +0530

    Added garbage collector program

---
+++ src/test/java/com/qa/garbageCollection/TestGarbage.java
@@ -0,0 +1,24 @@
+package com.qa.garbageCollection;
+
+public class TestGarbage {
+	
+	public void finalize()
+	{
+		System.out.println("Object is garbage collected");
+	}
+
+	public static void main(String[] args) {
+		
+		TestGarbage tg1=new TestGarbage();
+		TestGarbage tg2=new TestGarbage();
+		
+		tg1=null;
+		tg2=null;
+		System.gc();
+		
+		
+		
+
+	}
+
+}
